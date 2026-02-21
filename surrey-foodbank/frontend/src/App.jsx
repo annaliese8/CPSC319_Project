c9ff99d1b17bd7c4cf8c0344c201771f2ab915dd
Nina <nineranger@gmail.com>
2026-02-20 18:29:18 -0800

    Added functionality to add/remove (temp variables) blocked slots by clicking ONLY (no drag yet) + save choices

--- surrey-foodbank/frontend/src/App.jsx
+++ surrey-foodbank/frontend/src/App.jsx
@@ -4,7 +4,10 @@ import theme from "./styles/theme";
 import { Routes, Route } from "react-router-dom";
 import StaffLogin from "./pages/staff/Login";
 import StaffHome from "./pages/staff/Home";
+import EditSlot from "./pages/staff/EditSlot";
 import StaffApplicantInfo from "./pages/staff/ApplicantInfo";
+
+
 import ApplicantLogin from "./pages/applicant/Login";
 import ApplicantHome from "./pages/applicant/Home";
 import ApplicantCreateAccount from "./pages/applicant/CreateAccount";
@@ -19,6 +22,7 @@ function App() {
           {/* Staff routes */}
           <Route path="/staff/login" element={<StaffLogin />} />
           <Route path="/staff/home" element={<StaffHome />} />
+          <Route path="/staff/edit" element={<EditSlot />} />
           <Route
             path="/staff/applicant-info"
             element={<StaffApplicantInfo />}
---
+++ surrey-foodbank/frontend/src/pages/staff/EditSlot.jsx
@@ -0,0 +1,119 @@
+import React, { useState } from "react";
+import "./EditSlot.css";
+
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+// generate time slots from 9am – 5pm (30 min slots for now)
+const generateTimeSlots = () => {
+  const slots = [];
+  for (let hour = 0; hour < 24; hour++) {
+    slots.push(`${hour}:00`);
+    slots.push(`${hour}:15`);
+  }
+  return slots;
+};
+
+const generateInitBlockedSlots = () => {
+    const blockedSlots = [];
+    days.forEach((day) => {
+        for (let hour = 0; hour < 9; hour++) {
+            blockedSlots.push([day,`${hour}:00`]);
+            blockedSlots.push([day,`${hour}:15`]);
+        }
+        for (let hour = 15; hour < 24; hour++) {
+            blockedSlots.push([day,`${hour}:00`]);
+            blockedSlots.push([day,`${hour}:15`]);
+        }
+    })
+    console.log(blockedSlots);
+    return blockedSlots;
+};
+
+const timeSlots = generateTimeSlots();
+
+function EditSlot() {
+    const [blockedSlots, setBlockedSlots] = useState(generateInitBlockedSlots);
+    const addBlockedSlot = (slot) => {
+        setBlockedSlots((prevSlots) => [...prevSlots, slot]);
+    }
+
+    const removeBlockedSlot = (slot) => {
+        setBlockedSlots((prevSlots) => prevSlots.filter(s => s[0] !== slot[0] || s[1] !== slot[1]));
+    }
+
+
+  const [editing, setEditing] = useState(false);
+
+  const handleSave = () => {
+      setEditing(false);
+  };
+
+  const handleEditMode = () => {
+      setEditing(true);
+  };
+
+    const isBlocked = (day,time) => {
+        return blockedSlots.some(arr =>
+           arr.every((val, index) => val === [day,time][index])
+        );
+    }
+
+  return (
+    <div className="booking-container">
+      {/* Calendar */}
+      <div className="calendar">
+        <div className="calendar-header">
+          <div className="time-column"></div>
+          {days.map((day) => (
+            <div key={day} className="day-header">
+              {day}
+            </div>
+          ))}
+        </div>
+
+          {timeSlots.map((time) => (
+              <div key={time} className="calendar-row">
+                  <div className="time-label">{time}</div>
+                  {days.map((day) => {
+                      const isAvailable = Math.random() > 0.4; // random availability
+                      if(editing) {
+                          return (
+                              <div
+                                  key={day + time}
+                                  className={`slot ${isBlocked(day,time)? "unavailable-vis" : isAvailable? "available": "booked"}`}
+                                  onClick={() =>
+                                      !isBlocked(day,time)? addBlockedSlot([day,time]) : removeBlockedSlot([day,time])
+                                  }
+                              ></div>
+                          );
+                      } else {
+                          return (
+                              <div
+                                  key={day + time}
+                                  className={`slot ${isBlocked(day,time)? "unavailable-invis" : isAvailable? "available": "booked"}`}
+                              >
+                              </div>
+                          );
+                      }
+                  })}
+              </div>
+          ))}
+
+      </div>
+
+      {/* Booking Panel */}
+      <div className="side-panel">
+        <h3>Appointment Listing</h3>
+        {editing ? (
+          <>
+            <button onClick={handleSave}>Confirm Changes</button>
+          </>
+        ) : (
+            <button onClick={handleEditMode}>Edit Slots</button>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default EditSlot;
