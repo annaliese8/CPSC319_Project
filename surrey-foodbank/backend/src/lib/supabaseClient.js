8f6ad37efca6f9bddcd7e0e61e79714651282fd5
annaliese8 <annalieseferchau@gmail.com>
2026-03-24 20:43:20 -0700

    fixed dependencies and server so backend can successfully run

--- surrey-foodbank/backend/src/lib/supabaseClient.js
+++ surrey-foodbank/backend/src/lib/supabaseClient.js
@@ -1,8 +1,11 @@
-// obtained from https://supabase.com/docs/reference/javascript
-
+import dotenv from 'dotenv'
+import { fileURLToPath } from 'url'
+import { dirname, resolve } from 'path'
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.VITE_SUPABASE_URL
-const supabaseKey = process.env.VITE_SUPABASE_ANON_KEY
+const __dirname = dirname(fileURLToPath(import.meta.url))
+dotenv.config({ path: resolve(__dirname, '../../.env') })
 
+const supabaseUrl = process.env.SUPABASE_URL
+const supabaseKey = process.env.SUPABASE_ANON_KEY
 export const supabase = createClient(supabaseUrl, supabaseKey)
